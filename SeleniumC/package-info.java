35f8f462a3182ee9275ef1e79ef3abe707451654
Admin <Admin@DESKTOP-GVLMBEC>
2023-11-30 12:30:24 +0530

    commit msg

---
+++ SeleniumC/package-info.java
@@ -0,0 +1,123 @@
+package SeleniumC;
+ public class Object 
+
+	public static void main(String[] args) {
+		
+		class Chair{
+			//characteristics:- type,color,pattern,height,weight
+			// B- sitting purpose()
+			
+			   
+			String type; 
+			String color;
+                        double height;
+			double weight;
+		        String pattern;
+			
+			void sitting purpose(String content) {
+			}
+		
+                 class Laptop{
+                              //characteristics:- brand,processor,size,screen type
+			      // B- for working()
+   
+                            String brand;
+			    double processor;
+			    double size;
+                            String screen type ;
+                            void for working {(String content) {
+                             }
+
+                  class Mobile{
+                         
+                        //characteristics:- type, price,color,features
+			// B:- for connecting peoples()
+                          String type;
+                          int price;
+                          String color;
+                          String features;
+                          void for connecting peoples {(String content) {
+                           }
+     
+                  class Pen{
+                                //characteristics:- type, price,color
+			        // B:- for writing()
+
+                                    String type; 
+                                    String color;
+                                    int price; 
+                                 void for writing {(String content) {
+                             }
+                               
+                  class TV{
+                           //characteristics:- type, price,height
+			        // B:- for entertainment()
+                                  String type;
+                                  int price;  
+                                  double height; 
+                                 void for writing {(String content) {
+                             }  
+
+                  class Oven{
+                               //characteristics:- type, price,height,color,weight
+			        // B:- for cooking()
+                                   String type;
+                                  int price;  
+                                  double height; 
+                                  double weight;
+                                  String color;
+                                  void for writing {(String content) {
+                             }
+
+    	 	class Speaker{
+				//characteristics:- type, price,color
+			        // B- for listening()
+
+				String type;
+                                  int price; 
+				  String color;
+				 void for writing {(String content) {
+                             }
+		
+	       class Book{
+				
+				//characteristics:- genre,type,price,author,pages
+			        // B- for reading()
+					
+					String genre;
+					String type;
+					String author;
+					int pages;
+					int price;
+					
+				 void for reading {(String content) {
+                          	  
+					}
+
+     		class Car{
+				
+				//characteristics:- type,price,color,features,height
+			        // B- for Travel()
+				
+				 String type;
+                                  int price;  
+                                  double height; 
+                                  String color;
+					
+				 void for reading {(String content) {
+                          	  
+					}
+
+           class student{
+			//characteristics:- name,age,gender,standard,section
+			// B- study() 
+				
+				  String name;
+				  int age;
+				  String gender;
+				  int standard;
+				  String section;
+
+				void study {(String content) {
+                          	  
+					}
